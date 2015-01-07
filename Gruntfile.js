27ea83a6b861c83a4b0974c72f2fbc25ce6bb9e8
JP Given <jgiven@gmail.com>
2015-01-06 19:26:25 -0800

    Good check in place.

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>-<%=pkg.version%>.min.js'
+      }
+    },
+    wiredep: {
+      task: {
+        src: [
+        'index.html'
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-wiredep');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
