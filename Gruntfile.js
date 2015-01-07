8ee406af12290161a812d0b3e27bae9e95a0bccb
JP Given <jgiven@gmail.com>
2015-01-06 20:03:22 -0800

    More work. Getting closer. Wiredep, usemin, etc all working properly.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,28 +3,58 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>-<%=pkg.version%>.min.js'
-      }
-    },
     wiredep: {
       task: {
         src: [
-        'index.html'
+          'index.html'
         ]
+      }
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'build',
+        flow: {
+          html: {
+            steps: {
+              js: ['concat', 'uglifyjs'],
+              css: ['cssmin']
+            },
+            post: {}
+          }
+        }
+      }
+    },
+    rename: {
+      css: {
+        files: [{
+          src: ['build/app.css'],
+          dest: 'build/<%= pkg.name %>-<%= pkg.version %>.min.css'
+        }]
+      },
+      js: {
+        files: [{
+          src: ['build/app.js'],
+          dest: 'build/<%= pkg.name %>-<%= pkg.version %>.min.js'
+        }]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-rename');
   grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-usemin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', [
+    'useminPrepare',
+    'concat',
+    'uglify',
+    'cssmin',
+    'rename'
+  ]);
 
 };
